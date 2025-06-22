a92ac7a3afbe8bc75d665dd00e171ebfc858dc4b
ostolaza <i.ostolaza@gmail.com>
2025-06-22 01:34:48 -0700

    Fix: Update models/contact

--- models/Contact.js
+++ models/Contact.js
@@ -2,18 +2,13 @@
 //Contact.js
 
 const mongoose = require('mongoose');
+
 const ContactSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   message: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
-module.exports = mongoose.model('Contact', ContactSchema);
+module.exports = mongoose.models.Contact || mongoose.model('Contact', ContactSchema);
 
 
-// models/UnsubscribedEmail.js
-const UnsubscribedEmailSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  unsubscribedAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('UnsubscribedEmail', UnsubscribedEmailSchema);
